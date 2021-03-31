8b31647f4fa5b24859e821594dd505300dd44079
Vaishnavi <vaishdw2207@gmail.com>
2021-03-31 15:02:36 +0530

    feat: add dev tooling

---
+++ src/App.js
@@ -0,0 +1,30 @@
+const Item = (props) => {
+  return React.createElement("div", {}, [
+    React.createElement("h2", {}, props.name),
+    React.createElement("h3", {}, props.price),
+    React.createElement("h3", {}, prop.brand),
+  ]);
+};
+
+const App = () => {
+  return React.createElement("div", {}, [
+    React.createElement("h1", {}, "Escasuma!"),
+    React.createElement(Item, {
+      name: "Chic A-line Skirt",
+      price: "Rs. 2,078",
+      brand: "Vero Moda",
+    }),
+    React.createElement(Item, {
+      name: "Peplum Skirt",
+      price: "Rs. 778",
+      brand: "Only",
+    }),
+    React.createElement(Item, {
+      name: "Boho Top",
+      price: "Rs. 1,078",
+      brand: "Mango",
+    }),
+  ]);
+};
+
+ReactDOM.render(React.createElement(App), document.getElementById("root"));
