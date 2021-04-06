c262a3aadde3c65364230d58dbcef965b6ced273
Vaishnavi <vaishdw2207@gmail.com>
2021-04-07 00:53:34 +0530

    chore: fix prettier

--- src/App.js
+++ src/App.js
@@ -1,17 +1,14 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import Item from "./Item";
-
+import React from "react"
+import ReactDOM from "react-dom"
+import SearchParams from "./SearchParams"
 
 const App = () => {
   return (
     <div>
       <h1>Escasuma</h1>
-      <Item name="Peplum Skirt" price="Rs. 2,569" brand="Mango" />
-      <Item name="Boho Top" price="Rs. 1,896" brand="Vero Moda" />
-      <Item name="A-line Skirt" price="Rs. 1,574" brand="Forever 21" />
+      <SearchParams />
     </div>
-  );
-};
+  )
+}
 
-ReactDOM.render((<App/>), document.getElementById("root"));
+ReactDOM.render(<App />, document.getElementById("root"))
--- src/Item.js
+++ src/Item.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React from "react"
 
 // const Item = (props) => {
 //     return React.createElement("div", {}, [
@@ -9,13 +9,13 @@ import React from "react";
 //   };
 
 const Item = (props) => {
-  return (
-    <div>
-      <h2>{props.name}</h2>
-      <h3>{props.price}</h3>
-      <h3>{props.brand}</h3>
-    </div>
-  );
-};
+    return (
+        <div>
+            <h2>{props.name}</h2>
+            <h3>{props.price}</h3>
+            <h3>{props.brand}</h3>
+        </div>
+    )
+}
 
-export default Item;
+export default Item
